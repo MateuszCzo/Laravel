a101e84515b1acda1f0f9a6e59c92255c61c7186
Mateusz Czsonyka <mateuszczosn@gmail.com>
2023-03-25 12:52:20 +0100

    Zabezpieczenie strony - CSRF TOKEN

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import './bootstrap';
 
 import jQuery from 'jquery';
 window.$ = jQuery;
-
+window.$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
 import sweetAlert2 from 'sweetalert2';
 window.Swal = sweetAlert2;
